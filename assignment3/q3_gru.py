ac330dccfcbce4419be6d12d6fefd7bdfbbb6986
nanliha <nanlihao@gmail.com>
2018-05-13 16:09:00 +1000

    unkwown issue of gradient clip

--- assignment3/q3_gru.py
+++ assignment3/q3_gru.py
@@ -87,6 +87,8 @@ class SequencePredictor(Model):
 
         x = self.inputs_placeholder
         ### YOUR CODE HERE (~2-3 lines)
+        output, state = tf.nn.dynamic_rnn(cell, x, dtype = tf.float32)
+        preds = tf.sigmoid(state)
         ### END YOUR CODE
 
         return preds #state # preds
@@ -108,7 +110,7 @@ class SequencePredictor(Model):
         y = self.labels_placeholder
 
         ### YOUR CODE HERE (~1-2 lines)
-
+        loss = tf.reduce_mean(tf.nn.l2_loss(y - preds))
         ### END YOUR CODE
 
         return loss
@@ -142,7 +144,11 @@ class SequencePredictor(Model):
         optimizer = tf.train.GradientDescentOptimizer(learning_rate=self.config.lr)
 
         ### YOUR CODE HERE (~6-10 lines)
-
+        gradients, variables = zip(*optimizer.compute_gradients(loss))
+        if self.config.clip_gradients == True:
+            gradients, _ = tf.clip_by_global_norm(gradients, self.config.max_grad_norm)
+        self.grad_norm = tf.global_norm(gradients)
+        train_op = optimizer.apply_gradients(zip(gradients, variables))
         # - Remember to clip gradients only if self.config.clip_gradients
         # is True.
         # - Remember to set self.grad_norm
